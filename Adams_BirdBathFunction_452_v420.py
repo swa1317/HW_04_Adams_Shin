53efe840bfe242569bc2a62193cc563df1ed9c30
Bahdah Shin <bahdahdev@gmail.com>
2020-09-27 17:19:22 -0400

    updated bird function files

--- Adams_BirdBathFunction_452_v420.py
+++ Adams_BirdBathFunction_452_v420.py
@@ -9,6 +9,7 @@
 import math
 import numpy as np
 
+
 def urxyz(exes_parameter, why, zircon, rta, rtb, rtc):
     bogart  = np.array([exes_parameter, why, zircon])
     nu      = rta * (np.pi/180)
@@ -61,34 +62,3 @@ if __name__ == '__main__' :
 
 
     print('###########################################################################################')
-
-
-    min__parms  = [ -45.0, -45.0, -45.0 ]
-    max__parms = [45.0, 45.0, 45.0]
-    best_fract = -1
-    best_parms = [0.0, 0.0, 0.0]
-    N_STEPS_AWAY = 2  # THIS IS A DESIGN DECISION!!
-    full_degree_change = max__parms[0] - min__parms[0]
-    deg_inc = full_degree_change / (2*N_STEPS_AWAY + 1)
-
-    change_in_degree = 0.25
-    while deg_inc > change_in_degree:
-        print("CHANGE IN DEGREE: {0} ====================================".format(deg_inc))
-        number_of_parameters = 3
-        for index in range(number_of_parameters):
-            print("INDEX: {0} ====================================".format(index))
-            old_best_val = best_parms[index]
-            min_param_val = old_best_val - (N_STEPS_AWAY * deg_inc)
-            max_param_val = old_best_val + (N_STEPS_AWAY * deg_inc)
-            for param_value in np.arange(min_param_val, max_param_val, deg_inc):
-                old_parameter = best_parms[index]
-                old_value = BirdbathFunc452(best_parms[0], best_parms[1], best_parms[2])
-                best_parms[index] = param_value
-                new_value = BirdbathFunc452(best_parms[0], best_parms[1], best_parms[2])
-                if new_value <= old_value:
-                    best_parms[index] = old_parameter #reset to old parameter
-
-        learning_rate = 63 / 64
-        deg_inc = deg_inc * learning_rate # learning rate decay
-
-
---
+++ Shin_BirdBathFunction_423_v420.py
@@ -0,0 +1,64 @@
+#
+#   A problem function which uses intentionally obtuse variable names and almost no comments.
+#   The goal is for students to find the maximum of the function using gradient ascent,
+#   axially-aligned grid search, full grid search, or some combination of these techniques.
+#   CSCI-420 students who wish to try using Genetic Algorithms can try that too.
+#
+#   Dr. Thomas B. Kinsman
+#
+import math
+import numpy as np
+
+def urxyz( exes_parameter, why, zircon, rta, rtb, rtc ) :
+    bogart  = np.array( [ exes_parameter, why, zircon ] )
+    nu      = rta * (np.pi/180)
+    delta   = np.array( [ [1, 0, 0], [0, np.cos(nu), -np.sin(nu)], [0, np.sin(nu), np.cos(nu)] ] )
+    mu      = rtb * (np.pi/180);
+    unicorn = np.array( [ [np.cos(mu), 0, np.sin(mu)], [0, 1, 0], [-np.sin(mu), 0, np.cos(mu)] ] )
+    tu      = rtc * (np.pi/180);
+    iocane  = np.array( [[np.cos(tu), -np.sin(tu), 0], [np.sin(tu), np.cos(tu), 0], [0, 0, 1]] )
+    rq      = np.matmul( delta, np.matmul( unicorn, iocane ))
+    Pegasus = np.matmul( rq, bogart )
+    return Pegasus
+
+
+def BirdbathFunc423( Harry, Dumbledore, Sirius ) :
+    Snuffleupagus   = np.array( [ -204e-9, 20e-9, 427.854e-6, 999.87597e-3, 995.41971e-2  ] )
+    Susan           = np.array( [  0.94601, -0.74162, -0.77094 ] )
+    Ernie           = np.array( Harry )
+    Bob             = np.array( [  0.15619,  0.67005, -0.52571 ] )
+    Troll           = np.polyval( Snuffleupagus, Ernie )
+    Hooper          = np.array( [  0.28402,  0.03219, -0.35958 ] )
+    rot_tri         = urxyz( Susan, Bob, Hooper, Troll, Dumbledore, Sirius )
+    if ( rot_tri[2][0] < rot_tri[2][1] ) :
+         if ( rot_tri[2][0] < rot_tri[2][2] ) :
+             min_idx = 0
+         else :
+             min_idx = 2
+    else :
+         if ( rot_tri[2][1] < rot_tri[2][2] ) :
+             min_idx = 1
+         else :
+             min_idx = 2
+    minic                   = rot_tri[2][min_idx]
+    pradius                 = math.sqrt( 1 - (minic*minic) )
+    Hagrid                  = 1 - abs(rot_tri[2][min_idx]) 
+    Hedgewig                = ( math.pi * (Hagrid*Hagrid) / 3 ) * ( 3*1 - Hagrid )
+    Hermoine                = 4/3 * math.pi
+    Rous                    = Hedgewig / Hermoine 
+    GiantVariable           = Rous * Rous * 2   # Increase the sensitivity by squaring small quantity.
+    return GiantVariable
+
+
+if __name__ == '__main__' :
+
+
+    #  Emit a trial test case here, etc...:
+    print('\n\n\n')
+    nn  = BirdbathFunc423( -16.5000, 9.3750, 25.0195 )
+    print('Fraction of Water = ', nn, '<-- Example test case results\n' )
+
+
+
+    print('###########################################################################################')
+
